fdc2cf3ea71b2e1e22c1f5d431a670df23fc4da9
Kannan <kannanlearningarea@gmail.com>
2019-09-21 22:00:35 +0530

    ng-For

--- learn-angular/src/app/servers/servers.component.ts
+++ learn-angular/src/app/servers/servers.component.ts
@@ -9,12 +9,14 @@ export class ServersComponent {
     allowNewServer = false;
     serverCreationStatus = '';
     serverName = 'Test Server';
+    servers = ['Test Server - 1', 'Test Server - 2'];
     constructor() {
         setTimeout(() => {
             this.allowNewServer = true;
         }, 2000);
     }
     onServerCreate() {
+        this.servers.push(this.serverName);
         this.serverCreationStatus = 'Server was created, server name is   ' + this.serverName;
     }
 }
