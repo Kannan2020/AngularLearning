cfa717704181efd0ad184bb021cb40b05481d4a2
Kannan <kannanlearningarea@gmail.com>
2019-09-21 23:16:18 +0530

    Added Recipe List

--- learn-angular/src/app/recipes/recipe-list/recipe-list.component.ts
+++ learn-angular/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a simple Description',
+      'https://www.tablefortwoblog.com/wp-content/uploads/2018/05/eggplant-parmesan-recipe-photos-tablefortwoblog-1.jpg'
+      ),
+      new Recipe(
+        'A Test Recipe',
+        'This is a simple Description',
+        'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'
+        )
+  ];
   constructor() { }
 
   ngOnInit() {
