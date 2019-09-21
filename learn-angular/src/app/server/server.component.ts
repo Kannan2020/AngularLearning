6883cb5e09f0cd2a371229faf21b1b8b088745d4
Kannan <kannanlearningarea@gmail.com>
2019-09-22 00:05:13 +0530

    Input with Alias

--- learn-angular/src/app/server/server.component.ts
+++ learn-angular/src/app/server/server.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ServerComponent implements OnInit {
   serverId = 10;
   serverStatus = 'Offline';
-  @Input() server: {name: string};
+  // tslint:disable-next-line: no-input-rename
+  @Input('serverInfo') server: {name: string};
   constructor() {
     this.serverStatus = Math.random() > 0.5 ?  'Online' : 'Offline';
    }
