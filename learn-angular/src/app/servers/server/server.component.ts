f79fdef9451eb9feabe1a270b4dd08c3baf829d8
Kannan <kannanlearningarea@gmail.com>
2019-10-02 18:38:49 +0530

    Added Resolver

--- learn-angular/src/app/servers/server/server.component.ts
+++ learn-angular/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 import { parseTemplate } from '@angular/compiler';
 
 @Component({
@@ -17,12 +17,15 @@ export class ServerComponent implements OnInit {
               private ruter: Router) { }
 
   ngOnInit() {
-    // + is converting string to number
-    const id = +this.route.snapshot.params.id;
-    this.server = this.serversService.getServer(id);
-    this.route.params.subscribe((params: Params) => {
-        this.server = this.serversService.getServer(+params.id);
+    this.route.data.subscribe((data: Data) => {
+      this.server = data.server;
     });
+    // // + is converting string to number
+    // const id = +this.route.snapshot.params.id;
+    // this.server = this.serversService.getServer(id);
+    // this.route.params.subscribe((params: Params) => {
+    //     this.server = this.serversService.getServer(+params.id);
+    // });
   }
   onEdit() {
     this.ruter.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
